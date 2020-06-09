5ec79cb1cd660e99fc4bc7121d28bbf1cf70c48f
Tom Ward <tom@madwort.co.uk>
2020-06-09 13:44:49 +0000

    Improve output from hubserver mode 2 script

--- madwort_auto_patcher_hubserver_mode_2.py
+++ madwort_auto_patcher_hubserver_mode_2.py
@@ -7,14 +7,15 @@ all_jacktrip_send_ports = client.get_ports('.*send.*')
 
 # remove all existing jacktrip connections (hubserver autopatcher)
 # TODO: only remove autopatched connections, not our own connections (HOW?)
+print("=== Disconnecting existing connections ===")
 for	receive_port in all_jacktrip_receive_ports:
-	print(receive_port.name)
 	send_ports = client.get_all_connections(receive_port)
 	for send_port in send_ports:
-		print(send_port)
+		print('disconnect', receive_port.name, 'from', send_port.name)
 		client.disconnect(receive_port, send_port)
 	
 # add some new jacktrip connections
+print("=== Creating new connections ===")
 # this should match hubserver autopatch mode 2
 for receive_port in all_jacktrip_receive_ports:
 	this_client = receive_port.name.split(':')[0]
