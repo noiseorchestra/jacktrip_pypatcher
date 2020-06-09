48eb7ee15a525d6e36ff42da205847f9900cdb85
Tom Ward <tom@madwort.co.uk>
2020-06-09 13:42:41 +0000

    Refactor & amend behaviour

--- madwort_auto_patcher.py
+++ madwort_auto_patcher.py
@@ -6,38 +6,34 @@ all_jacktrip_receive_ports = client.get_ports('.*receive.*')
 
 # remove all existing jacktrip connections (hubserver autopatcher)
 # TODO: only remove autopatched connections, not our own connections (HOW?)
+print("=== Disconnecting existing connections ===")
 for	receive_port in all_jacktrip_receive_ports:
 	print(receive_port.name)
 	send_ports = client.get_all_connections(receive_port)
 	for send_port in send_ports:
-		print(send_port)
+		print(send_port.name)
 		client.disconnect(receive_port, send_port)
 	
 # add some new jacktrip connections
+print("=== Creating new connections ===")
 jacktrip_receive_1_ports = client.get_ports('.*receive_1')
-all_jacktrip_send_ports = client.get_ports('.*send.*')
-print("client count: ", len(jacktrip_receive_1_ports))
-channel_1_receive_ports = jacktrip_receive_1_ports[:len(jacktrip_receive_1_ports)//2]
-channel_2_receive_ports = jacktrip_receive_1_ports[len(jacktrip_receive_1_ports)//2:]
+print("client count:", len(jacktrip_receive_1_ports))
 
-print(channel_1_receive_ports)
-print(channel_2_receive_ports)
-
-for receive_port in channel_1_receive_ports:
+for	receive_port in jacktrip_receive_1_ports:
 	this_client = receive_port.name.split(':')[0]
-	for send_port in all_jacktrip_send_ports:
-		# TODO: just do it with regexes?
-		if send_port.name.startswith(this_client):
-			continue
-		if send_port.name.endswith('send_1'):
-			print('connect ', receive_port, send_port)
-			client.connect(receive_port, send_port)
+	# get a list of target ip addresses to send this receive port to
+	send_targets = list(map(lambda x: x.name.split(':')[0],filter(lambda x: not x.name.startswith(this_client),jacktrip_receive_1_ports)))
+	print("client", this_client, "with send_targets:", send_targets)
+	channel_1_send_addresses = send_targets[:len(send_targets)//2]
+	channel_2_send_addresses = send_targets[len(send_targets)//2:]
 
-for receive_port in channel_2_receive_ports:
-	this_client = receive_port.name.split(':')[0]
-	for send_port in all_jacktrip_send_ports:
-		if send_port.name.startswith(this_client):
-			continue
-		if send_port.name.endswith('send_2'):
-			print('connect ', receive_port, send_port)
-			client.connect(receive_port, send_port)
+	print("Targets split into two lists: ", channel_1_send_addresses, " : ", channel_2_send_addresses)
+
+	for send_address in channel_1_send_addresses:
+		send_port = send_address + ':send_1'
+		print('connect chan1', receive_port.name, 'to', send_port)
+		client.connect(receive_port.name, send_port)
+	for send_address in channel_2_send_addresses:
+		send_port = send_address + ':send_2'
+		print('connect chan2', receive_port.name, 'to', send_port)
+		client.connect(receive_port.name, send_port)
