7c275cd9e7d697b616433e2c0e5f279d7f2ec3ee
Tom Ward <tom@madwort.co.uk>
2020-06-09 13:48:01 +0000

    Improve output & add edge case for two clients

--- madwort_auto_patcher.py
+++ madwort_auto_patcher.py
@@ -8,10 +8,9 @@ all_jacktrip_receive_ports = client.get_ports('.*receive.*')
 # TODO: only remove autopatched connections, not our own connections (HOW?)
 print("=== Disconnecting existing connections ===")
 for	receive_port in all_jacktrip_receive_ports:
-	print(receive_port.name)
 	send_ports = client.get_all_connections(receive_port)
 	for send_port in send_ports:
-		print(send_port.name)
+		print('disconnect', receive_port.name, 'from', send_port.name)
 		client.disconnect(receive_port, send_port)
 	
 # add some new jacktrip connections
@@ -24,8 +23,13 @@ for	receive_port in jacktrip_receive_1_ports:
 	# get a list of target ip addresses to send this receive port to
 	send_targets = list(map(lambda x: x.name.split(':')[0],filter(lambda x: not x.name.startswith(this_client),jacktrip_receive_1_ports)))
 	print("client", this_client, "with send_targets:", send_targets)
-	channel_1_send_addresses = send_targets[:len(send_targets)//2]
-	channel_2_send_addresses = send_targets[len(send_targets)//2:]
+	if len(send_targets) == 1:
+		print('Only two clients, duplicate it')
+		channel_1_send_addresses = send_targets
+		channel_2_send_addresses = send_targets
+	else:
+		channel_1_send_addresses = send_targets[:len(send_targets)//2]
+		channel_2_send_addresses = send_targets[len(send_targets)//2:]
 
 	print("Targets split into two lists: ", channel_1_send_addresses, " : ", channel_2_send_addresses)
 
