1f267be59e556212a5a6250b5a451a3a23ee5fbf
Tom Ward <tom@madwort.co.uk>
2020-06-16 10:19:46 +0000

    Add dry run mode to tomcount script

--- jacktrip_pypatcher.py
+++ jacktrip_pypatcher.py
@@ -1,6 +1,6 @@
 # Helper functions for autopatching JackTrip hubserver clients
 
-def disconnect_all(jack_client, my_port):
+def disconnect_all(jack_client, my_port, dry_run = False):
 	"""disconnect everything from a port"""
 	send_ports = jack_client.get_all_connections(my_port)
 	for send_port in send_ports:
@@ -11,29 +11,44 @@ def disconnect_all(jack_client, my_port):
 		print('disconnect', my_port.name, 'from', send_port.name)
 		jack_client.disconnect(my_port, send_port)
 
-def connect_to_centre(jack_client, receive, send):
+def connect_to_centre(jack_client, receive, send, dry_run = False):
 	"""connect pair of receive ports to the send ports, centre panned"""
+	if dry_run:
+		print("Connect centre", receive, "to", send)
+		return
 	jack_client.connect(receive + ':1', send + ':send_1')
 	jack_client.connect(receive + ':2', send + ':send_2')
 
-def connect_to_left(jack_client, receive, send):
+def connect_to_left(jack_client, receive, send, dry_run = False):
 	"""connect pair of receive ports to the send ports, left panned"""
+	if dry_run:
+		print("Connect left", receive, "to", send)
+		return
 	jack_client.connect(receive + ':1', send + ':send_1')
 	jack_client.connect(receive + ':2', send + ':send_1')
 
-def connect_to_right(jack_client, receive, send):
+def connect_to_right(jack_client, receive, send, dry_run = False):
 	"""connect pair of receive ports to the send ports, right panned"""
+	if dry_run:
+		print("Connect right", receive, "to", send)
+		return
 	jack_client.connect(receive + ':1', send + ':send_2')
 	jack_client.connect(receive + ':2', send + ':send_2')
 
-def connect_to_soft_left(jack_client, receive, send):
+def connect_to_soft_left(jack_client, receive, send, dry_run = False):
+	if dry_run:
+		print("Connect soft left", receive, "to", send)
+		return
 	jack_client.connect(receive + ':1', 'slight-left:Input (Left)')
 	jack_client.connect('slight-left:Output (Left)', send + ':send_1')
 
 	jack_client.connect(receive + ':2', 'slight-left:Input (Right)')
 	jack_client.connect('slight-left:Output (Right)', send + ':send_2')
 
-def connect_to_soft_right(jack_client, receive, send):
+def connect_to_soft_right(jack_client, receive, send, dry_run = False):
+	if dry_run:
+		print("Connect soft right", receive, "to", send)
+		return
 	jack_client.connect(receive + ':1', 'slight-right:Input (Left)')
 	jack_client.connect('slight-right:Output (Left)', send + ':send_1')
 
--- madwort_auto_patcher_tomcount.py
+++ madwort_auto_patcher_tomcount.py
@@ -3,6 +3,7 @@ import os
 import random
 import jacktrip_pypatcher as p
 
+dry_run = True
 jackClient = jack.Client('MadwortAutoPatcher')
 
 # number_of_voices = random.randint(1,5)
@@ -25,10 +26,19 @@ print("=== Creating new connections ===")
 mpg123_ports_all = list(map(lambda x: x.name.split(':')[0],jackClient.get_ports('mpg123.*:1')))
 random.shuffle(mpg123_ports_all)
 mpg123_ports = mpg123_ports_all[0:number_of_voices]
+
+if dry_run:
+	mpg123_ports = ['mpg123-one', 'mpg123-two']
+
 print("count count:", len(mpg123_ports))
 print('counts', mpg123_ports)
 
 jacktrip_clients = list(map(lambda x: x.name.split(':')[0],jackClient.get_ports('.*receive_1')))
+
+if dry_run:
+	jacktrip_clients = ['..ffff.192.168.0.1', '..ffff.192.168.0.2', '..ffff.192.168.0.3', '..ffff.192.168.0.4', '..ffff.192.168.0.5', '..ffff.192.168.0.6']
+	jacktrip_clients = jacktrip_clients[0:2]
+
 print("client count:", len(jacktrip_clients))
 print('clients', jacktrip_clients)
 
@@ -39,24 +49,26 @@ print('clients', jacktrip_clients)
 # tom@noiseaa1:~/ng-jackspa$ ./jackspa-cli -j slight-left -i '0:0:0:-0.5:0:0' /usr/lib/ladspa/inv_input.so 3301 &
 
 for	jacktrip_client in jacktrip_clients:
+	print("---")
+	print("JackTrip client", jacktrip_client)
 	if len(mpg123_ports) < 1:
 		os._exit(1)
 
 	if len(mpg123_ports) == 1:
-		p.connect_to_centre(jackClient, mpg123_ports[0], jacktrip_client)
+		p.connect_to_centre(jackClient, mpg123_ports[0], jacktrip_client, dry_run)
 
 	if len(mpg123_ports) >= 2:
-		p.connect_to_left(jackClient, mpg123_ports[0], jacktrip_client)
-		p.connect_to_right(jackClient, mpg123_ports[1], jacktrip_client)
+		p.connect_to_left(jackClient, mpg123_ports[0], jacktrip_client, dry_run)
+		p.connect_to_right(jackClient, mpg123_ports[1], jacktrip_client, dry_run)
 
 	if len(mpg123_ports) == 3:
-		p.connect_to_centre(jackClient, mpg123_ports[2], jacktrip_client)
+		p.connect_to_centre(jackClient, mpg123_ports[2], jacktrip_client, dry_run)
 
 	if len(mpg123_ports) >= 4:
-		p.connect_to_soft_left(jackClient, mpg123_ports[2], jacktrip_client)
-		p.connect_to_soft_right(jackClient, mpg123_ports[3], jacktrip_client)
+		p.connect_to_soft_left(jackClient, mpg123_ports[2], jacktrip_client, dry_run)
+		p.connect_to_soft_right(jackClient, mpg123_ports[3], jacktrip_client, dry_run)
 
 	if len(mpg123_ports) == 5:
-		p.connect_to_centre(jackClient, mpg123_ports[4], jacktrip_client)
+		p.connect_to_centre(jackClient, mpg123_ports[4], jacktrip_client, dry_run)
 
 os._exit(0)
