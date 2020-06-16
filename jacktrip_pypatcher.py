2ab02e4981f94ffb2ecf5fd67648851ff00afaac
Tom Ward <tomward@fmail.co.uk>
2020-06-16 15:28:18 +0000

    Bugfix module
    * still using mpg123 port names, not jacktrip port names!

--- jacktrip_pypatcher.py
+++ jacktrip_pypatcher.py
@@ -16,32 +16,32 @@ def connect_to_centre(jack_client, receive, send, dry_run = False):
 	if dry_run:
 		print("Connect centre", receive, "to", send)
 		return
-	jack_client.connect(receive + ':1', send + ':send_1')
-	jack_client.connect(receive + ':2', send + ':send_2')
+	jack_client.connect(receive + ':receive_1', send + ':send_1')
+	jack_client.connect(receive + ':receive_2', send + ':send_2')
 
 def connect_to_left(jack_client, receive, send, dry_run = False):
 	"""connect pair of receive ports to the send ports, left panned"""
 	if dry_run:
 		print("Connect left", receive, "to", send)
 		return
-	jack_client.connect(receive + ':1', send + ':send_1')
-	jack_client.connect(receive + ':2', send + ':send_1')
+	jack_client.connect(receive + ':receive_1', send + ':send_1')
+	jack_client.connect(receive + ':receive_2', send + ':send_1')
 
 def connect_to_right(jack_client, receive, send, dry_run = False):
 	"""connect pair of receive ports to the send ports, right panned"""
 	if dry_run:
 		print("Connect right", receive, "to", send)
 		return
-	jack_client.connect(receive + ':1', send + ':send_2')
-	jack_client.connect(receive + ':2', send + ':send_2')
+	jack_client.connect(receive + ':receive_1', send + ':send_2')
+	jack_client.connect(receive + ':receive_2', send + ':send_2')
 
 def connect_to_ladspa(jack_client, receive, ladspa, dry_run = False):
 	"""connect a pair of receive ports to a ladspa plugin"""
 	if dry_run:
 		print("Connect to ladspa", receive, "to", ladspa)
 		return
-	jack_client.connect(receive + ':1', ladspa + ':Input (Left)')
-	jack_client.connect(receive + ':2', ladspa + ':Input (Right)')
+	jack_client.connect(receive + ':receive_1', ladspa + ':Input (Left)')
+	jack_client.connect(receive + ':receive_2', ladspa + ':Input (Right)')
 
 def connect_from_ladspa(jack_client, ladspa, send, dry_run = False):
 	"""connect a ladspa plugin to a pair of send ports"""
@@ -55,18 +55,18 @@ def connect_to_soft_left(jack_client, receive, send, dry_run = False):
 	if dry_run:
 		print("Connect soft left", receive, "to", send)
 		return
-	jack_client.connect(receive + ':1', 'slight-left:Input (Left)')
+	jack_client.connect(receive + ':receive_1', 'slight-left:Input (Left)')
 	jack_client.connect('slight-left:Output (Left)', send + ':send_1')
 
-	jack_client.connect(receive + ':2', 'slight-left:Input (Right)')
+	jack_client.connect(receive + ':receive_2', 'slight-left:Input (Right)')
 	jack_client.connect('slight-left:Output (Right)', send + ':send_2')
 
 def connect_to_soft_right(jack_client, receive, send, dry_run = False):
 	if dry_run:
 		print("Connect soft right", receive, "to", send)
 		return
-	jack_client.connect(receive + ':1', 'slight-right:Input (Left)')
+	jack_client.connect(receive + ':receive_1', 'slight-right:Input (Left)')
 	jack_client.connect('slight-right:Output (Left)', send + ':send_1')
 
-	jack_client.connect(receive + ':2', 'slight-right:Input (Right)')
+	jack_client.connect(receive + ':receive_2', 'slight-right:Input (Right)')
 	jack_client.connect('slight-right:Output (Right)', send + ':send_2')
