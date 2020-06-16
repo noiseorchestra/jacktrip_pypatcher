3e98b19e6dde3659b968771c549f96d03f204cf7
Tom Ward <tomward@fmail.co.uk>
2020-06-16 15:12:27 +0000

    Add some simple plugin start/stop scripts
    * Please adapt these for your own system!

---
+++ start_plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NG_JACKSPA_PATH=.
+
+$NG_JACKSPA_PATH/jackspa-cli -j right-30 -i '0:0:0:0.3:0:0' /usr/lib/ladspa/inv_input.so 3301 &
+$NG_JACKSPA_PATH/jackspa-cli -j right-50 -i '0:0:0:0.5:0:0' /usr/lib/ladspa/inv_input.so 3301 &
+$NG_JACKSPA_PATH/jackspa-cli -j right-65 -i '0:0:0:0.65:0:0' /usr/lib/ladspa/inv_input.so 3301 &
+$NG_JACKSPA_PATH/jackspa-cli -j left-30 -i '0:0:0:-0.3:0:0' /usr/lib/ladspa/inv_input.so 3301 &
+$NG_JACKSPA_PATH/jackspa-cli -j left-50 -i '0:0:0:-0.5:0:0' /usr/lib/ladspa/inv_input.so 3301 &
+$NG_JACKSPA_PATH/jackspa-cli -j left-65 -i '0:0:0:-0.65:0:0' /usr/lib/ladspa/inv_input.so 3301 &
