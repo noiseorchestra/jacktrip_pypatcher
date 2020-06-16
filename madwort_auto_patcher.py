6869ee555640278d26e9919fb68c9daee29e2862
Tom Ward <tom@madwort.co.uk>
2020-06-16 10:23:41 +0000

    Code re-use in hard patch script

--- madwort_auto_patcher.py
+++ madwort_auto_patcher.py
@@ -1,5 +1,7 @@
 import jack
+import jacktrip_pypatcher as p
 
+dry_run = True
 jackClient = jack.Client('MadwortAutoPatcher')
 
 all_jacktrip_receive_ports = jackClient.get_ports('.*receive.*')
@@ -8,15 +10,7 @@ all_jacktrip_receive_ports = jackClient.get_ports('.*receive.*')
 # TODO: only remove autopatched connections, not our own connections (HOW?)
 print("=== Disconnecting existing connections ===")
 for	receive_port in all_jacktrip_receive_ports:
-	send_ports = jackClient.get_all_connections(receive_port)
-	for send_port in send_ports:
-		# Do not disconnect from jack_capture ports.
-		# They do auto-reconnect, but we do not need to, and 
-		# the disconnection is not reliable
-		if send_port.name.startswith('jack_capture'):
-			continue
-		print('disconnect', receive_port.name, 'from', send_port.name)
-		jackClient.disconnect(receive_port, send_port)
+	p.disconnect_all(jackClient, receive_port)
 	
 # add some new jacktrip connections
 print("=== Creating new connections ===")
