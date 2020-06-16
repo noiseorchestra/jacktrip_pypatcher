62be32984d630a7a2220c440d0c61f1ccd01e8a1
Tom Ward <tomward@fmail.co.uk>
2020-06-16 15:28:52 +0000

    Add soft pan and loopback mode for two clients
    * this allows us to test the ladspa plugin usage with only two clients
    * also include some bugfixes!

--- madwort_auto_patcher.py
+++ madwort_auto_patcher.py
@@ -3,22 +3,26 @@ import os
 import random
 import jacktrip_pypatcher as p
 
-dry_run = True
+dry_run = False
 jackClient = jack.Client('MadwortAutoPatcher')
 
 # number_of_voices = random.randint(1,5)
 number_of_voices = 5
 
 all_jacktrip_receive_ports = jackClient.get_ports('.*receive.*')
-all_ladspa_ports = jackClient.get_ports('slight-.*')
+all_left_ladspa_ports = jackClient.get_ports('left-.*')
+all_right_ladspa_ports = jackClient.get_ports('right-.*')
 
 # remove all existing jacktrip connections (hubserver autopatcher)
 # TODO: only remove autopatched connections, not our own connections (HOW?)
 print("=== Disconnecting existing connections ===")
-for  receive_port in all_jacktrip_receive_ports:
+for receive_port in all_jacktrip_receive_ports:
   p.disconnect_all(jackClient, receive_port)
 
-for  ladspa_port in all_ladspa_ports:
+for ladspa_port in all_left_ladspa_ports:
+  p.disconnect_all(jackClient, ladspa_port)
+
+for ladspa_port in all_right_ladspa_ports:
   p.disconnect_all(jackClient, ladspa_port)
 
 # add some new jacktrip connections
@@ -30,7 +34,7 @@ if dry_run:
   jacktrip_clients = ['..ffff.192.168.0.1', '..ffff.192.168.0.2',
                       '..ffff.192.168.0.3', '..ffff.192.168.0.4',
                       '..ffff.192.168.0.5', '..ffff.192.168.0.6']
-  jacktrip_clients = jacktrip_clients[0:5]
+  jacktrip_clients = jacktrip_clients[0:2]
 
 print("client count:", len(jacktrip_clients))
 print('clients', jacktrip_clients)
@@ -51,8 +55,26 @@ if len(jacktrip_clients) == 1:
   p.connect_to_centre(jackClient, hold_music_port, jacktrip_clients[0], dry_run)
 
 if len(jacktrip_clients) == 2:
-  p.connect_to_centre(jackClient, jacktrip_clients[1], jacktrip_clients[0], dry_run)
-  p.connect_to_centre(jackClient, jacktrip_clients[0], jacktrip_clients[1], dry_run)
+  soft_pan_and_loopback = True
+
+  if soft_pan_and_loopback:
+    ladspa_soft_left = 'left-65'
+    ladspa_soft_right = 'right-65'
+    p.connect_to_ladspa(jackClient, jacktrip_clients[0], ladspa_soft_left, dry_run)
+    p.connect_to_ladspa(jackClient, jacktrip_clients[1], ladspa_soft_right, dry_run)
+
+    # client 0 - loopback panned left
+    p.connect_from_ladspa(jackClient, ladspa_soft_left, jacktrip_clients[0], dry_run)
+    # client 0 - client 1 panned right
+    p.connect_from_ladspa(jackClient, ladspa_soft_left, jacktrip_clients[1], dry_run)
+
+    # client 1 - client 0 panned left
+    p.connect_from_ladspa(jackClient, ladspa_soft_right, jacktrip_clients[0], dry_run)
+    # client 1 - loopback panned right
+    p.connect_from_ladspa(jackClient, ladspa_soft_right, jacktrip_clients[1], dry_run)
+  else:
+    p.connect_to_centre(jackClient, jacktrip_clients[1], jacktrip_clients[0], dry_run)
+    p.connect_to_centre(jackClient, jacktrip_clients[0], jacktrip_clients[1], dry_run)
 
 if len(jacktrip_clients) == 3 or len(jacktrip_clients) == 4:
   p.connect_to_left(jackClient, jacktrip_clients[1], jacktrip_clients[0], dry_run)
