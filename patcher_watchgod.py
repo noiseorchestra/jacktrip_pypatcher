700e8de2c761787dce6a6bc8e87cd68eecd28557
Tom Ward <tomward@fmail.co.uk>
2020-09-03 14:39:31 +0000

    Shorten check time for watchgod
    * getting frustrated testing connections - want it to respond quicker!

--- patcher_watchgod.py
+++ patcher_watchgod.py
@@ -3,15 +3,13 @@ import time
 import sys
 from watchgod import run_process
 
-
 def foobar():
     while True:
         try:
             jtp.main()
-            time.sleep(3600)
+            time.sleep(60)
         except Exception as e:
             print("pypatcher could not start:", e)
             sys.exit("Exited because of pypatcher error")
-
 
 run_process('/var/tmp/jacktrip_pypatcher', foobar)
