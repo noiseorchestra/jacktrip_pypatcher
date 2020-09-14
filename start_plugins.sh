6529e612d36e0ca032d5664c43e2b68248c3c856
sandreae <sandreae@googlemail.com>
2020-09-14 11:55:10 +0100

    Add hard left and right ladspa plugins

--- start_plugins.sh
+++ start_plugins.sh
@@ -11,7 +11,9 @@ $NG_JACKSPA_PATH/jackspa-cli -j left-20 -i '0:0:0:-0.2:0:0' /usr/lib/ladspa/inv_
 $NG_JACKSPA_PATH/jackspa-cli -j left-40 -i '0:0:0:-0.4:0:0' /usr/lib/ladspa/inv_input.so 3301 &
 $NG_JACKSPA_PATH/jackspa-cli -j left-60 -i '0:0:0:-0.6:0:0' /usr/lib/ladspa/inv_input.so 3301 &
 $NG_JACKSPA_PATH/jackspa-cli -j left-80 -i '0:0:0:-0.8:0:0' /usr/lib/ladspa/inv_input.so 3301 &
+$NG_JACKSPA_PATH/jackspa-cli -j left-100 -i '0:0:0:-1:0:0' /usr/lib/ladspa/inv_input.so 3301 &
 $NG_JACKSPA_PATH/jackspa-cli -j right-20 -i '0:0:0:0.2:0:0' /usr/lib/ladspa/inv_input.so 3301 &
 $NG_JACKSPA_PATH/jackspa-cli -j right-40 -i '0:0:0:0.4:0:0' /usr/lib/ladspa/inv_input.so 3301 &
 $NG_JACKSPA_PATH/jackspa-cli -j right-60 -i '0:0:0:0.6:0:0' /usr/lib/ladspa/inv_input.so 3301 &
 $NG_JACKSPA_PATH/jackspa-cli -j right-80 -i '0:0:0:0.8:0:0' /usr/lib/ladspa/inv_input.so 3301 &
+$NG_JACKSPA_PATH/jackspa-cli -j right-100 -i '0:0:0:1:0:0' /usr/lib/ladspa/inv_input.so 3301 &
