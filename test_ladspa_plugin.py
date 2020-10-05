d878b19597acdd6238cbe3f84c24e18c5bafb844
Tom Ward <tom@madwort.co.uk>
2020-10-05 22:19:49 +0000

    Merge two ladspa test files
    * a typo!

--- test_ladspa_plugin.py
+++ test_ladspa_plugin.py
@@ -96,3 +96,27 @@ def test_get_ports():
         )
         == panning_positions
     )
+
+
+def test_generate_subprocess_cmd(position=0.3):
+
+    panning_positions = [
+        0,
+        -0.15,
+        0.15,
+        -0.3,
+        0.3,
+        -0.45,
+        0.45,
+        -0.6,
+        0.6,
+        -0.75,
+        0.75,
+    ]
+
+    assert ladspa.get_panning_positions(2, panning_positions) == [
+        -0.45,
+        -0.46,
+        0.45,
+        0.46,
+    ]
