dd9116f7622eebc7d83615aa3309ef530853e621
sandreae <sandreae@googlemail.com>
2020-10-06 21:18:39 +0100

    Split start function for testing and pass in file prefix

--- stereo_recording.py
+++ stereo_recording.py
@@ -7,6 +7,20 @@ import subprocess
 # import time
 
 
+def get_command(recording_path_prefix):
+    return [
+        "jack_capture",
+        "--filename-prefix",
+        recording_path_prefix,
+        "-S",
+        "--channels",
+        "2",
+        "--port",
+        "darkice*",
+        "--daemon",
+    ]
+
+
 def darkice_process():
     for proc in psutil.process_iter(["pid", "name", "username", "cmdline"]):
         if proc.name() == "jack_capture":
@@ -15,31 +29,23 @@ def darkice_process():
                 return proc
 
 
-def start():
-    recording_path_prefix = "/home/sam/darkice-"
-
+def start(recording_path_prefix, dry_run):
+    if dry_run:
+        print("Start recording if not running already")
+        return
     print("-- recordings --")
     if darkice_process():
         print("already started!")
     else:
         print("starting...")
-        mysub = subprocess.Popen(
-            [
-                "jack_capture",
-                "--filename-prefix",
-                recording_path_prefix,
-                "-S",
-                "--channels",
-                "2",
-                "--port",
-                "darkice*",
-                "--daemon",
-            ]
-        )
+        mysub = subprocess.Popen(get_command(recording_path_prefix))
         print("Recording started, process num: ", mysub.pid)
 
 
-def stop():
+def stop(dry_run):
+    if dry_run:
+        print("Stop recording if not stopped already")
+        return
     print("-- recordings --")
     myprocess = darkice_process()
     if not myprocess:
