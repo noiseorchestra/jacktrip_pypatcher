8b20d0a4221e55458dff3e23511bbff8d51d7393
sandreae <sandreae@googlemail.com>
2020-10-09 21:18:59 +0100

    Write tests

--- test_jack_client_patching.py
+++ test_jack_client_patching.py
@@ -1,6 +1,32 @@
 import jack_client_patching as p
 
 
+def test_get_receive_string():
+    jackClient = None
+    jcp = p.JackClientPatching(jackClient, dry_run=True)
+
+    jacktrip_receive = jcp.get_receive_string("..ffff.192.168.0.1", "jacktrip")
+    ladspa_receive = jcp.get_receive_string("ladspa-left-30", "ladspa")
+    darkice_receive = jcp.get_receive_string("lounge-music", "mpg123")
+
+    assert jacktrip_receive == "..ffff.192.168.0.1:receive_.*"
+    assert ladspa_receive == "ladspa-left-30:Output.*"
+    assert darkice_receive == "lounge-music:.*"
+
+
+def test_get_send_string():
+    jackClient = None
+    jcp = p.JackClientPatching(jackClient, dry_run=True)
+
+    jacktrip_send = jcp.get_send_string("..ffff.192.168.0.1", "jacktrip")
+    ladspa_send = jcp.get_send_string("ladspa-left-30", "ladspa")
+    darkice_send = jcp.get_send_string("darkice", "darkice")
+
+    assert jacktrip_send == "..ffff.192.168.0.1:send_.*"
+    assert ladspa_send == "ladspa-left-30:Input.*"
+    assert darkice_send == "darkice:.*"
+
+
 def test_set_all_connections():
 
     jackClient = None
