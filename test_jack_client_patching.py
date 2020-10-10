a29ce24d4d53a9f9c6deb3d5f98a15dea400112a
Tom Ward <tomward@fmail.co.uk>
2020-10-10 13:54:44 +0100

    Add xfail test for 3 client patching (#68)
    Co-authored-by: Tom Ward <tom@madwort.co.uk>
    Co-authored-by: sandreae <sandreae@googlemail.com>

--- test_jack_client_patching.py
+++ test_jack_client_patching.py
@@ -1,4 +1,5 @@
 import jack_client_patching as p
+import pytest
 
 
 def test_set_all_connections():
@@ -34,6 +35,41 @@ def test_set_all_connections():
         ("ladspa-right-60:Output.*", "..ffff.192.168.0.1:send_.*"),
         ("ladspa-right-60:Output.*", "..ffff.192.168.0.2:send_.*"),
         ("ladspa-right-60:Output.*", "..ffff.192.168.0.3:send_.*"),
+    ]
+
+    jcp = p.JackClientPatching(jackClient, dry_run=True)
+    jcp.set_all_connections(jacktrip_clients, ladspa_ports)
+
+    assert jcp.connections == connections
+
+
+def test_set_all_connections_3_clients():
+    pytest.xfail("This configuration not currently supported")
+
+    jackClient = None
+    jacktrip_clients = [
+        "..ffff.192.168.0.1",
+        "..ffff.192.168.0.2",
+        "..ffff.192.168.0.3",
+    ]
+    ladspa_ports = [
+        "ladspa-left-45",
+        "ladspa-right-45",
+        "ladspa-left-46",
+        "ladspa-right-46",
+    ]
+
+    connections = [
+        ("..ffff.192.168.0.1:receive_.*", "ladspa-left-45:Input.*"),
+        ("ladspa-left-46:Output.*", "..ffff.192.168.0.1:send_.*"),
+        ("ladspa-right-45:Output.*", "..ffff.192.168.0.1:send_.*"),
+        ("..ffff.192.168.0.2:receive_.*", "ladspa-left-46:Input.*"),
+        ("..ffff.192.168.0.2:receive_.*", "ladspa-right-46:Input.*"),
+        ("ladspa-left-45:Output.*", "..ffff.192.168.0.2:send_.*"),
+        ("ladspa-right-45:Output.*", "..ffff.192.168.0.2:send_.*"),
+        ("..ffff.192.168.0.3:receive_.*", "ladspa-right-45:Input.*"),
+        ("ladspa-left-45:Output.*", "..ffff.192.168.0.3:send_.*"),
+        ("ladspa-right-46:Output.*", "..ffff.192.168.0.3:send_.*"),
     ]
 
     jcp = p.JackClientPatching(jackClient, dry_run=True)
