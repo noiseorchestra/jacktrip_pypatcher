ef7e00d3176a3e96b2fac44c404bc526c3815dbc
sandreae <sandreae@googlemail.com>
2020-10-10 14:01:00 +0100

    Update tests

--- test_jack_client_patching.py
+++ test_jack_client_patching.py
@@ -4,6 +4,79 @@ import pytest
 
 import jack_client_patching as p
 
+def test_jacktrip_connections_2_clients():
+
+    jackClient = None
+    jacktrip_clients = [
+        "..ffff.192.168.0.1",
+        "..ffff.192.168.0.2",
+    ]
+    ladspa_ports = [
+        "ladspa-left-45",
+        "ladspa-right-45",
+    ]
+
+    connections = [
+        ("..ffff.192.168.0.1:receive_.*", "..ffff.192.168.0.2:send_.*"),
+        ("..ffff.192.168.0.2:receive_.*", "..ffff.192.168.0.1:send_.*"),
+        ("..ffff.192.168.0.1:receive_.*", "ladspa-left-45:Input.*"),
+        ("..ffff.192.168.0.2:receive_.*", "ladspa-right-45:Input.*"),
+    ]
+
+    jcp = p.JackClientPatching(jackClient, dry_run=True)
+    jcp.set_all_connections(jacktrip_clients, ladspa_ports)
+
+    assert jcp.connections == connections
+
+def test_darkice_connections_2_clients():
+
+    jackClient = None
+    ladspa_ports = [
+        "ladspa-left-45",
+        "ladspa-right-45",
+    ]
+
+    darkice_port = "darkice"
+
+    connections = [
+        ("ladspa-left-45:Output.*", "darkice.*"),
+        ("ladspa-right-45:Output.*", "darkice.*"),
+    ]
+
+    jcp = p.JackClientPatching(jackClient, dry_run=True)
+    jcp.set_darkice_connections(ladspa_ports, darkice_port)
+
+    assert jcp.connections == connections
+
+def test_darkice_connections_2_clients():
+
+    jackClient = None
+    jacktrip_clients = [
+        "..ffff.192.168.0.1",
+        "..ffff.192.168.0.2",
+    ]
+    ladspa_ports = [
+        "ladspa-left-45",
+        "ladspa-right-45",
+    ]
+
+    darkice_port = "darkice"
+
+    connections = [
+        ("..ffff.192.168.0.1:receive_.*", "..ffff.192.168.0.2:send_.*"),
+        ("..ffff.192.168.0.2:receive_.*", "..ffff.192.168.0.1:send_.*"),
+        ("..ffff.192.168.0.1:receive_.*", "ladspa-left-45:Input.*"),
+        ("..ffff.192.168.0.2:receive_.*", "ladspa-right-45:Input.*"),
+        ("ladspa-left-45:Output.*", "darkice.*"),
+        ("ladspa-right-45:Output.*", "darkice.*"),
+    ]
+
+    jcp = p.JackClientPatching(jackClient, dry_run=True)
+    jcp.set_all_connections(jacktrip_clients, ladspa_ports)
+    jcp.set_darkice_connections(ladspa_ports, darkice_port)
+
+    assert jcp.connections == connections
+
 
 def test_jacktrip_connections():
 
