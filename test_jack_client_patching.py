e887f1aa1508c5d697770322eaf1c099469a762a
sandreae <sandreae@googlemail.com>
2020-10-10 19:56:32 +0100

    Update tests

--- test_jack_client_patching.py
+++ test_jack_client_patching.py
@@ -1,10 +1,9 @@
 import jack_client_patching as p
 import pytest
-
-
 import jack_client_patching as p
 
-def test_jacktrip_connections_2_clients():
+
+def test_set_jacktrip_connections_2_clients():
 
     jackClient = None
     jacktrip_clients = [
@@ -28,7 +27,8 @@ def test_jacktrip_connections_2_clients():
 
     assert jcp.connections == connections
 
-def test_darkice_connections_2_clients():
+
+def test_set_darkice_connections_2_clients():
 
     jackClient = None
     ladspa_ports = [
@@ -48,7 +48,8 @@ def test_darkice_connections_2_clients():
 
     assert jcp.connections == connections
 
-def test_darkice_connections_2_clients():
+
+def test_set_all_connections_2_clients():
 
     jackClient = None
     jacktrip_clients = [
@@ -78,7 +79,7 @@ def test_darkice_connections_2_clients():
     assert jcp.connections == connections
 
 
-def test_jacktrip_connections():
+def test_set_jacktrip_connections_4_clients():
 
     jackClient = None
     jacktrip_clients = [
@@ -119,7 +120,7 @@ def test_jacktrip_connections():
     assert jcp.connections == connections
 
 
-def test_darkice_connections():
+def test_set_darkice_connections_4_clients():
 
     jackClient = None
 
@@ -145,7 +146,7 @@ def test_darkice_connections():
     assert jcp.connections == connections
 
 
-def test_all_connections():
+def test_set_all_connections_4_clients():
 
     jackClient = None
     jacktrip_clients = [
@@ -189,6 +190,83 @@ def test_all_connections():
     jcp = p.JackClientPatching(jackClient, dry_run=True)
     jcp.set_all_connections(jacktrip_clients, ladspa_ports)
     jcp.set_darkice_connections(ladspa_ports, darkice_port)
+
+    assert jcp.connections == connections
+
+
+def test_set_jacktrip_connections_3_clients():
+
+    jackClient = None
+    jacktrip_clients = [
+        "..ffff.192.168.0.1",
+        "..ffff.192.168.0.2",
+        "..ffff.192.168.0.3",
+    ]
+    ladspa_ports = [
+        "ladspa-centre",
+        "ladspa-left-45",
+        "ladspa-right-45",
+        "ladspa-left-46",
+        "ladspa-right-46",
+    ]
+
+    ladspa_ports = [
+        "ladspa-centre",
+        "ladspa-left-45",
+        "ladspa-right-45",
+        "ladspa-left-46",
+        "ladspa-right-46",
+    ]
+
+    connections = [
+        ("..ffff.192.168.0.1:receive_.*", "ladspa-centre:Input.*"),
+        ("..ffff.192.168.0.1:receive_.*", "ladspa-left-45:Input.*"),
+        ("..ffff.192.168.0.1:receive_.*", "ladspa-right-45:Input.*"),
+        ("..ffff.192.168.0.2:receive_.*", "ladspa-left-46:Input.*"),
+        ("..ffff.192.168.0.3:receive_.*", "ladspa-right-46:Input.*"),
+        ("ladspa-left-46:Output.*", "..ffff.192.168.0.1:send_.*"),
+        ("ladspa-right-46:Output.*", "..ffff.192.168.0.1:send_.*"),
+        ("ladspa-left-45:Output.*", "..ffff.192.168.0.2:send_.*"),
+        ("ladspa-right-46:Output.*", "..ffff.192.168.0.2:send_.*"),
+        ("ladspa-left-46:Output.*", "..ffff.192.168.0.3:send_.*"),
+        ("ladspa-right-45:Output.*", "..ffff.192.168.0.3:send_.*"),
+    ]
+
+    jcp = p.JackClientPatching(jackClient, dry_run=True)
+    jcp.set_all_connections(jacktrip_clients, ladspa_ports)
+
+    assert jcp.connections == connections
+
+
+def test_set_darkice_connections_3_clients():
+
+    jackClient = None
+    ladspa_ports = [
+        "ladspa-centre",
+        "ladspa-left-45",
+        "ladspa-right-45",
+        "ladspa-left-46",
+        "ladspa-right-46",
+    ]
+
+    ladspa_ports = [
+        "ladspa-centre",
+        "ladspa-left-45",
+        "ladspa-right-45",
+        "ladspa-left-46",
+        "ladspa-right-46",
+    ]
+
+    darkice_port = "darkice"
+
+    connections = [
+        ("ladspa-centre:Output.*", "darkice.*"),
+        ("ladspa-left-46:Output.*", "darkice.*"),
+        ("ladspa-right-46:Output.*", "darkice.*"),
+    ]
+
+    jcp = p.JackClientPatching(jackClient, dry_run=True)
+    jcp.set_darkice_connections([ladspa_ports[0]] + ladspa_ports[3:], darkice_port)
 
     assert jcp.connections == connections
 
