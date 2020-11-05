e4eb08234d49ed20025ae91510c47a6a763c0e4a
sandreae <git@contact.samandreae.com>
2020-11-05 17:25:11 +0000

    Change recordings filename and path

--- test_stereo_recording.py
+++ test_stereo_recording.py
@@ -1,13 +1,25 @@
+from unittest.mock import Mock
 from stereo_recording import StereoRecording
 
+def test_get_filename_prefix():
+    stereo_recording = StereoRecording("/home/sam/recordings/audio/", dry_run=True)
+    mock = Mock()
+    mock.return_value = "2020_10_15_1254"
+    stereo_recording.get_time_string = mock
+    filename_prefix = "/home/sam/recordings/audio/2020_10_15_1254-"
+    assert stereo_recording.get_filename_prefix() == filename_prefix
 
 def test_generate_subprocess_cmd():
 
-    stereo_recording = StereoRecording("/home/sam/darkice-", dry_run=True)
+    stereo_recording = StereoRecording("/home/sam/recordings/audio/", dry_run=True)
+    mock = Mock()
+    mock.return_value = "2020_10_15_1254"
+    stereo_recording.get_time_string = mock
+
     cmd = [
         "jack_capture",
         "--filename-prefix",
-        "/home/sam/darkice-",
+        "/home/sam/recordings/audio/2020_10_15_1254-",
         "-S",
         "--channels",
         "2",
