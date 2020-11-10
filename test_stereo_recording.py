6229320093dd07ad853b2711bc9a1ffc240aeadd
Tom Ward <tom@madwort.co.uk>
2020-11-10 14:25:15 +0000

    Pass black

--- test_stereo_recording.py
+++ test_stereo_recording.py
@@ -2,9 +2,10 @@ from unittest.mock import Mock
 from stereo_recording import StereoRecording
 import pytest
 
+
 def test_get_filename_prefix(freezer):
     stereo_recording = StereoRecording("/home/sam/recordings/audio/", dry_run=True)
-    freezer.move_to('2020-10-15 12:54')
+    freezer.move_to("2020-10-15 12:54")
 
     filename_prefix = "/home/sam/recordings/audio/2020_10_15_1254-"
     assert stereo_recording.get_filename_prefix() == filename_prefix
@@ -13,7 +14,7 @@ def test_get_filename_prefix(freezer):
 def test_generate_subprocess_cmd(freezer):
 
     stereo_recording = StereoRecording("/home/sam/recordings/audio/", dry_run=True)
-    freezer.move_to('2020-10-15 12:54')
+    freezer.move_to("2020-10-15 12:54")
 
     cmd = [
         "jack_capture",
