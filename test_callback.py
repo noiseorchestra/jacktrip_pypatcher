585017308b1c7174924698391ca8528153778006
sandreae <git@contact.samandreae.com>
2021-02-04 17:07:31 +0000

    Tests

---
+++ test_callback.py
@@ -0,0 +1,11 @@
+from unittest.mock import Mock
+import patcher_callback
+
+def test_jacktrip_v1_2():
+    assert patcher_callback.check_for_jacktrip_client("...ffff.123.123.123.123")
+
+def test_jacktrip_v1_3():
+    assert patcher_callback.check_for_jacktrip_client("__ffff.123.123.123.123")
+
+def test_non_jacktrip_client():
+    assert patcher_callback.check_for_jacktrip_client("00ffff.123.123.123.123") == False
